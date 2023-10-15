1617a40b7b74d0e0167d40a89c7d58b5e67e8b33
Adrian <adrianshisia@gmail.com>
2023-10-15 12:28:46 +0300

    styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Home () {
     return (
-    <div >
-        <img src="https://media.istockphoto.com/id/1312991431/photo/creative-layout-of-colorful-riskus-leaves-on-a-white-background-in-the-rays-of-the-sun-with.jpg?b=1&s=612x612&w=0&k=20&c=kizzZWfOYfGNLoP6wxcJ9pUuwFvBJpV8WSRJKS5NaH4=" alt="product image" width="1300" /> 
+    <div class="vh-100">
+        <img class="h-100 w-100" src="https://media.istockphoto.com/id/1312991431/photo/creative-layout-of-colorful-riskus-leaves-on-a-white-background-in-the-rays-of-the-sun-with.jpg?b=1&s=612x612&w=0&k=20&c=kizzZWfOYfGNLoP6wxcJ9pUuwFvBJpV8WSRJKS5NaH4=" alt="product image"/> 
         <div class="one">
         <p class="fs-1 fw-bolder">Simply the best</p>
         <p class="fs-3">Search, Order, we Deliver</p>
